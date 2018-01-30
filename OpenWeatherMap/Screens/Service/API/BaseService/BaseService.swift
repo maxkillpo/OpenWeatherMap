7cc40f34469db04c7fa63a04295e02de503f21d1
Varodom Naunplub <varodom.naul@gmail.com>
2018-01-30 17:42:04 +0700

    remove validate response in class service function unsuccess

--- OpenWeatherMap/Screens/Service/API/BaseService/BaseService.swift
+++ OpenWeatherMap/Screens/Service/API/BaseService/BaseService.swift
@@ -7,13 +7,6 @@ class BaseService<Response: BaseResponseModel> {
     private let baseURL = URL(string: "http://api.openweathermap.org/data/2.5")!
     private let session = URLSession.shared
 
-    func validateResponse(response: Response) throws -> Observable<Response> {
-        guard response.cod == 200 else {
-            throw OpenWeatherMapError.responseErrorNon200
-        }
-        return Observable.of(response)
-    }
-
     func buildRequest(method: String = "GET", pathComponent: String, params: [String: Any]) -> Observable<Response> {
 
         let url = baseURL.appendingPathComponent(pathComponent)
@@ -37,8 +30,6 @@ class BaseService<Response: BaseResponseModel> {
             .map { json in
                 let jsonMapper = JsonMapper()
                 return try jsonMapper.mapper(json.dictionaryObject)
-            }
-            .flatMap { response in
-                return try self.validateResponse(response: response) }
+        }
     }
 }
